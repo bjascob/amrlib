44881b7006998e4d42552c19d4702e05ee7a7e0f
bjascob <bjascob@msn.com>
2020-09-03 20:00:55 -0600

    Use full name for default spacy model

--- amrlib/defaults.py
+++ amrlib/defaults.py
@@ -6,4 +6,4 @@ data_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.realpath(os.path.join(data_dir, 'data'))
 
 # Spacy model name
-spacy_model_name = 'en'     # en_core_web_sm
+spacy_model_name = 'en_core_web_sm'
